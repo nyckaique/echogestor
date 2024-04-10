beefa1ad56966a2acb0be348a3b7aeba6be8b9b2
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-04-10 11:46:39 -0300

    fix loading schedule delay at home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import Title from "../../components/Title";
 import HomeIcon from "@mui/icons-material/Home";
 import BemVindo from "../../components/BemVindo";
+import { Navigate } from "react-router-dom";
 import "./home.css";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
   const [qtdAgendamentos, setQtdAgendamentos] = useState(0);
   const [agendamentosHoje, setAgendamentosHoje] = useState([]);
   const [faturamento, setFaturamento] = useState(0);
+  const [carregou, setCarregou] = useState(false);
 
   useEffect(() => {
     function filtroAgendamentosHoje() {
@@ -36,99 +38,106 @@ export default function Home() {
       setAgendamentosHoje(a);
       setQtdAgendamentos(a.length);
     }
-    filtroAgendamentosHoje();
-  }, []);
+    if (agendamentos) {
+      setCarregou(true);
+      filtroAgendamentosHoje();
+    }
+  }, [agendamentos]);
 
-  return (
-    <div>
-      <Header />
-      <Title name="Home">
-        <HomeIcon fontSize="large" />
-      </Title>
-      <BemVindo />
-      <div className="cardContent">
-        <div className="agendaCard">
-          <p>Hoje você tem</p>
-          <span>{qtdAgendamentos}</span>
-          <p>agendamentos</p>
+  if (!carregou) {
+    return <p>Carregando...</p>;
+  } else {
+    return (
+      <div>
+        <Header />
+        <Title name="Home">
+          <HomeIcon fontSize="large" />
+        </Title>
+        <BemVindo />
+        <div className="cardContent">
+          <div className="agendaCard">
+            <p>Hoje você tem</p>
+            <span>{qtdAgendamentos}</span>
+            <p>agendamentos</p>
+          </div>
+          <div className="agendaCard">
+            <p>Seu faturamento hoje</p>
+            <span>
+              {faturamento.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+          </div>
         </div>
-        <div className="agendaCard">
-          <p>Seu faturamento hoje</p>
-          <span>
-            {faturamento.toLocaleString("pt-br", {
-              style: "currency",
-              currency: "BRL",
-            })}
-          </span>
-        </div>
-      </div>
-      <h2 style={{ width: "100%", textAlign: "center" }}>
-        Agendamentos do dia
-      </h2>
-      <div className="agendaTable">
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Produto / Serviço</th>
-              <th>Horário</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {agendamentosHoje.map((agendamento, index) => {
-              let date = new Date(agendamento.date);
+        <h2 style={{ width: "100%", textAlign: "center" }}>
+          Agendamentos do dia
+        </h2>
+        <div className="agendaTable">
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Produto / Serviço</th>
+                <th>Horário</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {agendamentosHoje.map((agendamento, index) => {
+                let date = new Date(agendamento.date);
 
-              return (
-                <tr key={index}>
-                  <td>{agendamento.nomeCliente}</td>
-                  <td>
-                    {agendamento.nomeProduto}
-                    {": "}
-                    <b>
-                      {agendamento.valorProduto.toLocaleString("pt-br", {
-                        style: "currency",
-                        currency: "BRL",
+                return (
+                  <tr key={index}>
+                    <td>{agendamento.nomeCliente}</td>
+                    <td>
+                      {agendamento.nomeProduto}
+                      {": "}
+                      <b>
+                        {agendamento.valorProduto.toLocaleString("pt-br", {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </b>
+                    </td>
+                    <td>
+                      {date.toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
                       })}
-                    </b>
-                  </td>
-                  <td>
-                    {date.toLocaleTimeString([], {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                    {"h "}
-                    {date.toLocaleDateString()}
-                  </td>
-                  <td style={{ textAlign: "center" }}>
-                    {agendamento.status ? (
-                      <p
-                        style={{
-                          backgroundColor: "#5cb85c",
-                          borderRadius: "0.5em",
-                          padding: "0.25em",
-                        }}
-                      >
-                        Concluído
-                      </p>
-                    ) : (
-                      <p
-                        style={{
-                          backgroundColor: "#d9534f",
-                          borderRadius: "0.5em",
-                          padding: "0.25em",
-                        }}
-                      >
-                        Aguardando pagamento
-                      </p>
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                      {"h "}
+                      {date.toLocaleDateString()}
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      {agendamento.status ? (
+                        <p
+                          style={{
+                            backgroundColor: "#5cb85c",
+                            borderRadius: "0.5em",
+                            padding: "0.25em",
+                          }}
+                        >
+                          Concluído
+                        </p>
+                      ) : (
+                        <p
+                          style={{
+                            backgroundColor: "#d9534f",
+                            borderRadius: "0.5em",
+                            padding: "0.25em",
+                          }}
+                        >
+                          Aguardando pagamento
+                        </p>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
